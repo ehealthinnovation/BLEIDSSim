09f864d2d62bbf636d9fa1102b37138e78ed3f8d
Kevin Tallevi <ktallevi@ehealthinnovation.org>
2017-09-21 10:11:21 -0400

    fixed byte order in packets

--- ids.py
+++ ids.py
@@ -51,6 +51,33 @@ class OperationalState(object):
 	waiting = 0x66
 	ready = 0x96
 
+class OpCodes(object):
+	response_code = 0x0303
+	reset_status = 0x030C
+	get_active_bolus_ids = 0x0330
+	get_active_bolus_ids_response = 0x033F
+	get_active_bolus_delivery = 0x0356
+	get_active_bolus_delivery_response = 0x0359
+	get_active_basal_rate_delivery = 0x0365
+	get_active_basal_rate_delivery_response = 0x036A
+	get_total_daily_insulin_status = 0x0395
+	get_total_daily_insulin_status_response = 0x039A
+	get_counter = 0x03A6
+	get_counter_response = 0x03A9
+	get_delivered_insulin = 0x03C0
+	get_delivered_insulin_response = 0x03CF
+	get_insulin_on_board = 0x03F3
+	get_insulin_on_board_response = 0x03FC
+
+class ResponseCodes(object):
+	success = 0x0F
+	op_code_not_supported = 0x70
+	invalid_operand = 0x71
+	procedure_not_supported = 0x72
+	parameter_out_of_range = 0x73
+	procedure_not_applicable = 0x74
+
+
 def ids_init():
 	global crc_count
 	crc_count = int()
@@ -63,13 +90,17 @@ def get_ids_status():
 	
 	status = [therapy, operationalState]
 	reservoirRemainingAmount = float_to_shortfloat(5.22)
-	status.append(reservoirRemainingAmount >> 8)
 	status.append(reservoirRemainingAmount & 0xff)
+	status.append(reservoirRemainingAmount >> 8)
 
+	# reservoir attached
+	status.append(1)
+
+	# crc-counter
 	status.append(0)
 	crc = crc_calculate(status)
-	status.append(crc >> 8)
 	status.append(crc & 0xff)
+	status.append(crc >> 8)
 
 	return status
 
@@ -94,9 +125,9 @@ def get_ids_status_changed():
 	status = [ids_status >> 8, ids_status & 0xff]
 	status.append(0)
 	crc = crc_calculate(status)
-	status.append(crc >> 8)
 	status.append(crc & 0xff)
-	
+	status.append(crc >> 8)
+		
 	return status
 
 
@@ -130,14 +161,17 @@ def get_ids_features():
 	features = [0x00, ids_features >> 8, ids_features & 0xff]
 	
 	shortfloat_value = float_to_shortfloat(5.22)
-	features.append(shortfloat_value >> 8)
+	
 	features.append(shortfloat_value & 0xff)
+	features.append(shortfloat_value >> 8)
+	
 	features.append(0)
 
 	crc = crc_calculate(features)
-	features.append(crc >> 8)
+	
 	features.append(crc & 0xff)
-
+	features.append(crc >> 8)
+	
 	return features
 
 
@@ -175,3 +209,17 @@ def get_ids_annunciation_status():
 	annunciation.append(crc & 0xff)
 
 	return annunciation
+
+
+def parse_ids_status_reader_control_point(value):
+	print('parse_ids_status_reader_control_point')
+	print(value)
+
+def parse_ids_command_control_point(value):
+	print('parse_ids_command_control_point')
+	print(value)
+
+def parse_racp(value):
+	print("parse_racp")
+	print(value)
+
