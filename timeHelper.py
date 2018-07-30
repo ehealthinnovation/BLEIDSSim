65944b292186c85c9552db717fc049ceea330641
Kevin Tallevi <ktallevi@ehealthinnovation.org>
2018-07-30 11:06:12 -0400

    added DST to reference time

--- timeHelper.py
+++ timeHelper.py
@@ -1,12 +1,11 @@
 import time
 from datetime import datetime
 
-#TO-DO: add proper DST value
 def current_time_to_hex():
 	now = datetime.now()
 	year_hex = hex(now.year)[2:].zfill(4)
 	time_zone_byte = int((time.altzone / 3600) * 60)
-	time_data = ''.join(reversed([year_hex[i:i+2] for i in range(0, len(year_hex), 2)])) + hex(now.month)[2:].zfill(2) + hex(now.day)[2:].zfill(2) + hex(now.hour)[2:].zfill(2) + hex(now.minute)[2:].zfill(2) + hex(now.second)[2:].zfill(2) + hex(time_zone_byte)[2:] + '00'
+	time_data = ''.join(reversed([year_hex[i:i+2] for i in range(0, len(year_hex), 2)])) + hex(now.month)[2:].zfill(2) + hex(now.day)[2:].zfill(2) + hex(now.hour)[2:].zfill(2) + hex(now.minute)[2:].zfill(2) + hex(now.second)[2:].zfill(2) + hex(time_zone_byte)[2:] + str(time.daylight).zfill(2)
 	return time_data
 
 def date_hex_to_datetime(value):
